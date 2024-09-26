77fd3380821e9724d939a5c46804bcc402a1f66d
kbliss86 <132328278+kbliss86@users.noreply.github.com>
2024-09-25 20:04:16 -0500

    added project folder

---
+++ c-oop-project-1-pos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_oop_project_1_pos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
