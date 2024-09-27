2e15d7efbf1ab349686427fc29a433fb02c36fd5
kbliss86 <132328278+kbliss86@users.noreply.github.com>
2024-09-27 02:36:46 -0500

    moved generate and display list methods to product class, adjust the list displays to look more like a table and subtotals to "cart" list

--- c-oop-project-1-pos/Program.cs
+++ c-oop-project-1-pos/Program.cs
@@ -9,78 +9,68 @@ using System.Threading.Tasks;
 namespace c_oop_project_1_pos
 {
     internal class Program
-    {
-        static CommonlyUsedFunctions myFuncs = new CommonlyUsedFunctions();
+    {    /************************************************************************************
+         * Initialize Elements
+         ************************************************************************************/
+        static CommonlyUsedFunctions myFuncs = new CommonlyUsedFunctions();//Connect to Commonly Used Functions
         static List<Product> productList = new List<Product>();//list to be filled out by GenerateProductList()
         static List<Product> cart = new List<Product> ();//List to generate as user adds items to card
+        /************************************************************************************
+         * Main Program
+         ************************************************************************************/
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the point of sale terminal");
-            GenerateProductList();//Product.GenerateProductList
 
-            //prompt user to pick an item(by number) on a loop
-            //while loop
-            do
+            Console.WriteLine("Welcome to the point of sale terminal");//Welcome Message
+            productList = Product.GenerateProductList();//Generate Product List
+            do//do-while loop - prompt user to pick an item by the item number in the list
             {
-                //while loop for displaying menu - ask user if they want to see the menu again after selecting they want to add more items
-                DisplayProductList();//ADjust once method is moved
-                Console.WriteLine("Please Select an Item # to add to your cart: ");
-                int item = myFuncs.GetANumber();
-                Console.WriteLine("How many do you want to add to your cart?");
-                int quantity = myFuncs.GetANumber();
-                for (int i = 0; i < quantity; i++)
+                /************************************************************************************
+                * Menu Display
+                ***********************************************************************************/
+                bool displayProducts = true;//bool variable for if the user needs to see the menu again - ask user if they want to see the menu again after selecting they want to add more items
+                if (cart.Count > 0)//if cart count is greater than one - ask question - if not then skip this and display the menu
+                    {
+                        //Ask user to see if they want to view the menu
+                        displayProducts = myFuncs.moreMenu();
+                    }
+                    if (displayProducts)//if user wants to look at menu - display it - otherwise skip it and just add more stuff
                 {
-                    cart.Add(productList[item - 1]);
+                    myFuncs.WriteSeparatorLine("Product Menu");//Display Product Menu
+                    Product.DisplayProductList();
                 }
-                myFuncs.WriteSeparatorLine("Shopping Cart");
-                foreach (Product currentProduct in cart)
-                {
-                    Console.WriteLine($"{currentProduct.IdNum}. {currentProduct.Name} Price: ${currentProduct.Price}");
-                }
+                /************************************************************************************
+                * Add Items to Card
+                ***********************************************************************************/
+                Console.WriteLine("Please Select an Item # from the menu to add to your cart: ");//Prompt user to select an item
+                int item = myFuncs.GetANumber();//Item that will be added
+                Console.WriteLine("How many do you want to add to your cart?");//Prompt user for quantity
+                int quantity = myFuncs.GetANumber();//Quantitity of items to be added
+                    for (int i = 0; i < quantity; i++)//Loop to add quantity of items user selected
+                    {
+                        cart.Add(productList[item - 1]);//add to list using IdNum
+                    }
+                /************************************************************************************
+                * Display Items in Card and Running Sub Totals
+                ***********************************************************************************/
+                myFuncs.WriteSeparatorLine("Shopping Cart");//Display Shopping Card
+                Product.DisplayCartList(cart);//Display Contents of Cart from the method in Product class
             }
-            while (myFuncs.moreInput());
+                while (myFuncs.moreInput()) ;//ASk user if the want to add more items
 
+            /************************************************************************************
+            * Ask Payment Section
+            ***********************************************************************************/
             myFuncs.WriteSeparatorLine("Payment Section");
             //prompt user to select payment type - use a switch statment and run different class methods with polymorphism
 
+            /************************************************************************************
+            * Display Reciept
+            ***********************************************************************************/
             myFuncs.WriteSeparatorLine("Reciept Section");
-            //Display Reciept - use a switch statement to display different reciepts based on payment type chosen using polymorphism
+                //Display Reciept - use a switch statement to display different reciepts based on payment type chosen using polymorphism
 
-        }
-        //Move the methods to Product Class
-        //Change to generate from csv file -- maybe move to product class to keep main clean
-
-        static void GenerateProductList()
-        {
-            StreamReader productReader = new StreamReader("../../Random_Grocery_List.csv");
-            string line = "";
-            while ((line = productReader.ReadLine()) != null)
-            {
-                if (string.IsNullOrWhiteSpace(line))
-                    continue;
-
-                var values = line.Split(',');
-                productList.Add(new Product(int.Parse(values[0]), (string)values[1], (string)values[2], (string)values[3], double.Parse(values[4])));
-
-                if (values.Length != 5)
-                {
-                    Console.WriteLine($"Invalid line: {line}");
-                    continue;
-                }               
-            }           
-        }
-        //Potentially move to product class if necessary
-        static void DisplayProductList()
-        {
-            foreach (Product currentProduct in productList)
-            {
-                Console.WriteLine($"{currentProduct.IdNum}. {currentProduct.Name} Price: ${currentProduct.Price}\nCategory: {currentProduct.Category}" +
-                    $"\nDescription: {currentProduct.Description}");
-            }
-        }
-
-        //Make a Display Cart List Method - Put in product as well
-
-
-    }
-}
+            
+         }//End Main
+    }//End Class
+}//End NameSpace
